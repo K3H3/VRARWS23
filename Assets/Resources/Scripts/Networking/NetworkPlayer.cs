2ea0daa662d571ffbe24e4cca811af7a14d87603
K3H3 <62655106+K3H3@users.noreply.github.com>
2023-11-11 15:02:04 +0100

    Retry setting up network (still not working)
    Problem might be in NetworkPlayer script (XRRig != XROrigin)  https://www.youtube.com/watch?v=DB5bajOMdUQ

---
+++ Assets/Resources/Scripts/Networking/NetworkPlayer.cs
@@ -0,0 +1,80 @@
+using Photon.Pun;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+    public PhotonView photonView;
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        XROrigin rig = FindObjectOfType<XROrigin>();
+
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        leftHandRig = rig.transform.Find("Camera Offset/Left Controller");
+        rightHandRig = rig.transform.Find("Camera Offset/Right Controller");
+
+        if (photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+        }
+    }
+
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void MapPosition(Transform target, Transform rigTransform)
+    {
+        target.position = rigTransform.position;
+        target.rotation = rigTransform.rotation;
+    }
+}
