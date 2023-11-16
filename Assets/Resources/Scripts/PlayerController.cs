d5d551ae507344e4299be42f7e1a87efa0d90a35
K3H3 <62655106+K3H3@users.noreply.github.com>
2023-11-16 22:07:34 +0100

    added player controller

---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+  
+            Debug.Log("Player collided with the special floor!");
+            // Your code here for actions when the player collides with the special floor
+        
+    }
+
+}
