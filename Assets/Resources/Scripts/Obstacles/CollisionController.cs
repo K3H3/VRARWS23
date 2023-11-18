d7c1a51292bc138dfc7ca8220ac90ee59ae0f84a
Leon Meka <leonmeka@gmail.com>
2023-11-18 17:28:54 +0100

    fix: make collider follow main camera

--- Assets/Resources/Scripts/Obstacles/CollisionController.cs
+++ Assets/Resources/Scripts/Obstacles/CollisionController.cs
@@ -3,6 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class CollisionController : MonoBehaviour
 {
+    public Transform targetPosition;
+
+    void Update()
+    {
+        if (targetPosition != null)
+        {
+            transform.position = new Vector3(targetPosition.position.x, 0, targetPosition.position.z);
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Obstacle"))
