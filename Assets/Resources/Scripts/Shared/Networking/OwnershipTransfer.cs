bb58f36990f3d7f0f9c7c4fb8c02b61a4ebf1e38
K3H3 <62655106+K3H3@users.noreply.github.com>
2023-11-23 23:53:35 +0100

    feat: add OwnershipTransfer script

---
+++ Assets/Resources/Scripts/Shared/Networking/OwnershipTransfer.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class OwnershipTransfer : MonoBehaviourPun, IPunOwnershipCallbacks
+{
+    private InputDevice targetDevice;
+
+    private void Awake()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDestroy()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
+    }
+
+    private void Update()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.triggerButton,
+                out var triggerValue))
+        {
+            if (triggerValue)
+                photonView.RequestOwnership();
+        }
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        if (targetView != photonView)
+        {
+            return;
+        }
+
+        photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        if (targetView != photonView)
+        {
+            return;
+        }
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        Debug.LogError("Ownership Transfer Failed");
+    }
+}
