215b038c7aa9b1e9bcff8623dc510631ce422acd
K3H3 <62655106+K3H3@users.noreply.github.com>
2023-11-28 18:32:07 +0100

    feat: add static ui + merge HandUI into XR Setup

--- Assets/Resources/Scripts/Project/Controller/GameController.cs
+++ Assets/Resources/Scripts/Project/Controller/GameController.cs
@@ -1,11 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public GameObject target;
     public float magnitude = 100.0f;
+    private float _startTime;
+    public Text timeText;
 
-    private Vector3 direction = new Vector3(-1,0,0);
+    private Vector3 direction = new Vector3(-1, 0, 0);
+
+    public void Start()
+    {
+        _startTime = Time.time;
+    }
+    
+    private void Update()
+    {
+        UpdateTime();
+    }
+    
+    private void UpdateTime()
+    {
+        float elapsedTime = Time.time - _startTime;
+        string formattedTime = string.Format("{0:F0} seconds", elapsedTime);
+
+        if (timeText != null)
+        {
+            timeText.text = "Time spent: " + formattedTime;
+        }
+    }
 
     public void Play()
     {
@@ -21,7 +45,6 @@ public class GameController : MonoBehaviour
         {
             Debug.LogError("The 'target' GameObject does not have a Rigidbody component.");
             return;
-
         }
 
         rb.AddForce(direction.normalized * magnitude, ForceMode.Acceleration);
