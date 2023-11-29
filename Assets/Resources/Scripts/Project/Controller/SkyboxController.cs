8a202d20501654807c95605bf0c822296a15d4ac
Leon Meka <leonmeka@gmail.com>
2023-11-29 22:19:56 +0100

    fix: minor fixes

--- Assets/Resources/Scripts/Project/Controller/SkyboxController.cs
+++ Assets/Resources/Scripts/Project/Controller/SkyboxController.cs
@@ -6,14 +6,6 @@ public class SkyboxController : MonoBehaviour
     public Material nightSkybox;
     public Material sunsetSkybox;
 
-    void Start()
-    {
-        // Load skybox materials from Resources folder
-        daySkybox = Resources.Load<Material>("Skybox/Project/day");
-        nightSkybox = Resources.Load<Material>("Skybox/Project/night");
-        sunsetSkybox = Resources.Load<Material>("Skybox/Project/sunset");
-    }
-
     public void SetDaySkybox()
     {
         RenderSettings.skybox = daySkybox;
